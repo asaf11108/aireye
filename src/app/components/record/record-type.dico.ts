2189c4685b6d1e202364d6fa67bb406098db246c
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-05-27 22:53:45 +0300

    align record

---
+++ src/app/components/record/record-type.dico.ts
@@ -0,0 +1,8 @@
+// Translate record type to angular material icon
+export const RECORD_TYPES = {
+    accessPoint: 'router',
+    station: 'computer',
+    alert: 'warning',
+    event: 'notifications'
+
+};
--- src/app/components/record/record.component.ts
+++ src/app/components/record/record.component.ts
@@ -1,3 +1,4 @@
+import { RECORD_TYPES } from './record-type.dico';
 import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
 import { Record } from '../../interfaces/record';
 
@@ -8,8 +9,10 @@ import { Record } from '../../interfaces/record';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RecordComponent implements OnInit {
+  RECORD_TYPES = RECORD_TYPES;
+
   @Input() record: Record;
-  recordType = ['help', 'warning', 'computer', 'notifications', 'router'];
+
   constructor() { }
 
   ngOnInit(): void {

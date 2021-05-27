8f21f5d140436df896e68644511c666d6f9f6c3c
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-05-27 15:38:40 +0300

    request records

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { RecordsService } from './state/records.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'aireye';
+export class AppComponent implements OnInit {
+
+  constructor(private recordsService: RecordsService) {}
+  
+  ngOnInit(): void {
+    this.recordsService.get().subscribe();
+  }
 }

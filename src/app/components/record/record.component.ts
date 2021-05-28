570a188bccb789518c32af6fcdebe78a65faa2f1
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-05-28 14:52:54 +0300

    active border

--- src/app/components/record/record.component.ts
+++ src/app/components/record/record.component.ts
@@ -1,7 +1,11 @@
+import { Observable } from 'rxjs';
+import { RecordsQuery } from './../../state/records.query';
+import { RecordsStore } from './../../state/records.store';
 import { RECORD_TYPES } from './record-type.settings';
 import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
 import { Record } from '../../interfaces/record';
 import { RecordType } from '../../interfaces/record-type';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-record',
@@ -15,10 +19,20 @@ export class RecordComponent implements OnInit {
 
   @Input() record: Record;
 
-  constructor() { }
+  isActive$: Observable<boolean>;
+
+  constructor(
+    private recordsQuery: RecordsQuery,
+    private recordsStore: RecordsStore
+    ) { }
 
   ngOnInit(): void {
-    this.record.record_type
+    this.record.record_type;
+    this.isActive$ = this.recordsQuery.selectActiveId().pipe(map(activeId => activeId === this.record.id));
+  }
+
+  onRecordClick(): void {
+    this.recordsStore.setActive(this.record.id);
   }
 
 }
